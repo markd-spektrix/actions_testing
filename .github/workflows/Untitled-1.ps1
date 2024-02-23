0289e6840862240f93eddb018c4cad1fea222872
Mark Dodson <mark.dodson@spektrix.com>
2024-02-23 14:05:41 +0000

    Fixing

--- .github/workflows/Untitled-1.ps1
+++ .github/workflows/Untitled-1.ps1
@@ -1,6 +1,13 @@
-$doop = "{{ steps.plan.outputs.stdout }}"
+$doop = "
+{{ steps.plan.outputs.stdout }}
+
+ewqewq
+ewqewq
+No changes. Your infrastructure matches the configuration.
+ewqewq
+"
 echo $doop
-if ( ($doop).Contains("No changes. Your infrastructure matches the configuration.")) {
+if ( $doop.Contains("No changes. Your infrastructure matches the configuration.")) {
     echo "No Changes Planned -- Auto merge & Deploy"
 }
 else {
